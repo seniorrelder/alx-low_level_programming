72c392e3de35ccdd95cbc2f096953093d2c2cca7
Venwick M <bluebettle2@gmail.com>
2024-06-28 16:05:26 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+#include <stdio.h>
+
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
+
