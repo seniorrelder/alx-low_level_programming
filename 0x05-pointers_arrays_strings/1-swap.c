04a4352888e6266f42b21077feb64c04928560d4
seniorrelder <noelslayer2@gmail.com>
2024-07-01 15:46:46 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,10 +9,10 @@
  * Return: nothing
  */
 
-void swap_int(int*a, int *b)
+void swap_int(int *a, int *b)
 /* the function that swaps the values of two integers. */
 {
 	int temp = *a;
 	*a = *b;
-	*b = temp;
+	*b = temo;
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  _strlen 
+ *  @str; The string to get the length of 
+ *  Return: The length of @str
+ */
+size_t_strlen(const char *str)
+
+{
+	size_t length = 0
+
+		while (*str++)
+		length++
+			return (length);
+}
