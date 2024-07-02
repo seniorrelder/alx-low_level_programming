e2a71ea34f10f69d80265b301b4c106bf55de4fe
seniorrelder <noelslayer2@gmail.com>
2024-07-02 09:32:41 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,25 +1,3 @@
-#include <stdio.h>
-
-/**
- *  _strlen 
- *  @str; The string to get the length of 
- *  Return: The length of @str
- */
-size_t_strlen(const char *str)
-
-{
-	size_t length = 0
-
-		while (*str++)
-		length++
-			return (length);
-}
-
-
-
-
-
-
 #include <stddef.h>
 
 size_t size_t_strlen(const char *str) {
@@ -29,3 +7,4 @@ size_t size_t_strlen(const char *str) {
     }
     return length; // Ensure the function returns a value
 }
+
