b6cd3e5e710af6799fd31dacb3c4309bf3d8ecb6
seniorrelder <noelslayer2@gmail.com>
2024-07-02 12:09:54 -0700

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 
+void rev_string(char *s) {
+    int len = 0, index = 0;
+    char temp;
+
+    // Calculate the length of the string
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    // Reverse the string
+    for (index = 0; index < len / 2; index++) {
+        temp = s[index];
+        s[index] = s[len - index - 1];
+        s[len - index - 1] = temp;
+    }
+}
 
 /**
- *  rev_string - Reverses a string
- *  @s: The string to be modified
- *  Return: void
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-void rev_string(char *s)
+int main(void) {
+    char s[11] = "Holberton!";
 
-{
-
-	int lenc=c0, index = 0;
-	char tmp;
-
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= len / 2; index--)
-	{
-	tmp = s[index];
-	s[index] = s[len - index - 1];
-	s[len - index - 1] =tmp;
-	}
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
 }
