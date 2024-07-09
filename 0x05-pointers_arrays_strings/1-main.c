486acdc4b0d8bbb42583c143fc28e14653e14c09
seniorrelder <noelslayer2@gmail.com>
2024-07-08 21:14:33 -0700

    ./1-swap

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    *  Return: Always 0.
- *     /
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     int a;
-    int b; 
+    int b;
 
-             a = 98;
-             b = 42;
-             printf("a=%d, b=%d\n", a, b);
-             swap_int(&a, &b);
-             printf("a=%d, b=%d\n", a, b);
-             return (0);
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,12 @@
-
 #include <stdio.h>
 
-/**
- * swap_int - swaps the values of two integers.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swapped.
- *
- * Return: nothing
+/*
+ * swap_int - Swaps the values of two integers.
+ * @a: A pointer to the first integer to be swapped.
+ * @b: A pointer to the second integer to be swapped.
  */
-
-void swap_int(int *a, int *b)
-/* the function that swaps the values of two integers. */
-{
-	int temp = *a;
-	*a = *b;
-	*b = temo;
+void swap_int(int *a, int *b) {
+    int temp = *a; /* Temporarily store the value of *a */
+    *a = *b; /* Assign the value of *b to *a */
+    *b = temp; /* Assign the stored value of *a (temp) to *b */
 }
