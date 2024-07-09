37684eba0eaa489ef42fe777e411591d0274810e
seniorrelder <noelslayer2@gmail.com>
2024-07-08 21:06:55 -0700

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-  char src[40];
-  char dest[100];
-  
-  strcpy(src, "This is source."); /* Initialize src */
-  my_strcpy(dest, src); /* Copy src to dest */
-  
-  printf("Final copied string : %s\n", dest); /* Print the copied string */
-  
-  return 0;
+char *_strcpy(char *dest, char *src) {
+    char *start = dest;
+
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+    return start;
 }
