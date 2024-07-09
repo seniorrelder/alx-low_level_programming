411d8707cbec7c861f9e3068e1fc6f1f41ad8fb1
seniorrelder <noelslayer2@gmail.com>
2024-07-08 22:29:02 -0700

    3-main.c

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_numbers();
-    return (0);
+int _putchar(char c); /* Prototype for the _putchar function */
+
+void print_numbers(void) {
+    int num;
+    for (num = 0; num <= 9; num++) {
+        _putchar(num + '0');
+    }
+    _putchar('\n');
 }
