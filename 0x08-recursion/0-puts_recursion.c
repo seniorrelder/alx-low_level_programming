13bac421734de160b5000ba5a7745be77b7ae79f
seniorrelder <noelslayer2@gmail.com>
2024-07-10 11:21:47 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/* 
+/**
  * _puts_recursion - Prints a string followed by a new line, recursively.
  * @s: The string to print.
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')  /* Base case: if the current character is the null terminator */
+    if (*s == '\0') /* Base case: if the current character is the null terminator */
     {
-        putchar('\n');  /* Print a new line and return */
+        putchar('\n'); /* Print a new line and return */
         return;
     }
-    putchar(*s);  /* Print the current character */
-    _puts_recursion(s + 1);  /* Recursive call with the next character of the string */
+    putchar(*s); /* Print the current character */
+    _puts_recursion(s + 1); /* Recursive call with the next character of the string */
 }
