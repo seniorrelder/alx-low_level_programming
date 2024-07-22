bce71cf7995f0d96a4b82f5af5a13ab66e33f29a
seniorrelder <noelslayer2@gmail.com>
2024-07-22 11:10:03 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef SIZE_HEADER_H
+#define SIZE_HEADER_H
+
+#define SIZE 1024
+
+#endif /* SIZE_HEADER_H*/
+
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef PI_HEADER_H
+#define PI_HEADER_H
+
+#define PI 3.14159265359
+
+#endif /* PI_HEADER_H*/
